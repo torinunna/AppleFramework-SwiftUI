872380b0348fe48ca2f45c93b04b9a4550d0292f
torinunna <torinunna9588@gmail.com>
2023-06-17 16:29:22 +0900

    add grid inside scrollView, state property list hold the list of AppleFramework

--- AppleFramework-SwiftUI/FrameworkData.swift
+++ AppleFramework-SwiftUI/FrameworkData.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-struct AppleFramework {
+struct AppleFramework: Hashable, Identifiable {
+    var id: UUID = UUID()
+    
     let name: String
     let imageName: String
     let urlString: String
--- AppleFramework-SwiftUI/FrameworkListView.swift
+++ AppleFramework-SwiftUI/FrameworkListView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct FrameworkListView: View {
+    
+    @State var list: [AppleFramework] = AppleFramework.list
+    
+    let layout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(list) { item in
+                    FrameworkCell(framework: item)
+                }
+            }
+            .padding([.top, .leading, .trailing], 16.0)
+        }
     }
 }
 
