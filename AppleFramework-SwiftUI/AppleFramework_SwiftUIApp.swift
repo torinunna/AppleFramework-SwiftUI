5f5504fa23a98f89373f6e016da423041fd6e607
torinunna <torinunna9588@gmail.com>
2023-06-17 16:01:01 +0900

    Initial Commit

---
+++ AppleFramework-SwiftUI/AppleFramework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFramework_SwiftUIApp.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by YUJIN KWON on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct AppleFramework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
