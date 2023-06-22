d787850088d14d348d46e0884b6020788e7e0b5e
torinunna <torinunna9588@gmail.com>
2023-06-22 13:26:25 +0900

    add modifiers to FrameworkDetailView

--- AppleFramework-SwiftUI/FrameworkDetailView.swift
+++ AppleFramework-SwiftUI/FrameworkDetailView.swift
@@ -12,18 +12,32 @@ struct FrameworkDetailView: View {
     @StateObject var viewModel: FrameworkDetailViewModel
     
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
+            
+            Spacer()
             
             Image(viewModel.framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
             Text(viewModel.framework.name)
+                .font(.system(size: 24, weight: .bold))
             Text(viewModel.framework.description)
+                .font(.system(size: 16))
+            
+            Spacer()
             
             Button {
                 print("safari 띄우기")
             } label: {
                 Text("Learn More")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
             }
+            .frame(maxWidth: .infinity, minHeight: 80)
+            .background(.pink)
+            .cornerRadius(40)
         }
+        .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
     }
 }
 
